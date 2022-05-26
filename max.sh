1853f960b658526158ebf99b1baf6cc0ba11a87e
Albert <acruz37@csub.edu>
2022-05-26 11:54:43 -0700

    Max now reads lines from a file

--- max.sh
+++ max.sh
@@ -3,15 +3,24 @@
 # Algorithm 1 from Chapter 3 in the Rosen Discrete Structures textbook. It 
 # should perform a linear search to find the maximum value in an array.
 
-# Just some array
-array=(182 18 1 992 2 8 9 10 11 71 72)
-length=11
-max=${array[1]}
-N=0
-while ((N < length)); do
-	if ((max < ${array[N]})); then
-		max=${array[N]}
+# === INPUT VALIDATION ===
+# They need to pass the file name as the second argument. It must contain the
+# numbers to be processed, with \n delimiters.
+file=$1
+if ! [ -e "$file" ]; then
+	echo "$0: $file does not exist!"
+	exit 1
+fi
+
+# Preload max with the first element in the file using `head`
+max=$(head -n 1 $file)
+echo "Current max value is ${max}"
+# We don't need an index because you can iterate over lines in a file in bash
+while read -r line; do
+	echo "Next line in file is ${line}"
+	if (($line > $max)); then
+		echo "It is greater than ${max}, so it will replace it."
+		max=$line
 	fi
-	((N++))
-done
+done < "$file"
 echo "Max value in the array is ${max}."
